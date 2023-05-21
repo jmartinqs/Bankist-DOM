970a518dfed643d11f9f8f54a707fae597e4c72e
Martin Quevedo <martinqs@live.com.ar>
2023-05-20 21:31:08 -0300

    Events

--- script.js
+++ script.js
@@ -37,40 +37,40 @@ document.addEventListener('keydown', function (e) {
 ///// Selecting Elements
 
 /// querySelector
-document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-// console.log(allSections);
+// document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// // console.log(allSections);
 
-/// getElementBy
-document.getElementById('.section--1');
-const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
+// /// getElementBy
+// document.getElementById('.section--1');
+// const allButtons = document.getElementsByTagName('button');
+// // console.log(allButtons);
 
-/// getElementByClassName
-document.getElementsByClassName('btn');
+// /// getElementByClassName
+// document.getElementsByClassName('btn');
 
-///// Creating and inserting elements
-// .insertAdjacentHTML();
+// ///// Creating and inserting elements
+// // .insertAdjacentHTML();
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.textContent = 'We use cookies for analytics and personalized content.';
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for analytics and personalized content.';
 
-const closeBtn = document.createElement('button');
-closeBtn.classList.add('btn--close-cookie');
-closeBtn.textContent = 'Got it!';
-message.appendChild(closeBtn);
+// const closeBtn = document.createElement('button');
+// closeBtn.classList.add('btn--close-cookie');
+// closeBtn.textContent = 'Got it!';
+// message.appendChild(closeBtn);
 
-// append the message element to the body element
-document.body.appendChild(message);
+// // append the message element to the body element
+// document.body.appendChild(message);
 
-///// Deleting elements
-// message.remove() its recent
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// ///// Deleting elements
+// // message.remove() its recent
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
 
 // oldway
 // message.parentElement.removeChild(message);
@@ -126,25 +126,67 @@ document
 // // Dont use ,it would overwrite other classes
 // logo.className = 'Martin';
 
+////// Smooth scrolling ////////
+/// old school of doing smooth scroll
+
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
+
+//   console.log(e.target.getBoundingClientRect());
+
+//   console.log('Current scroll (x/y)', window.pageXOffset, pageYOffset);
+
+//   console.log(
+//     'Height/Width viewport',
+//     document.documentElement.clientHeight,
+//     document.documentElement.clientWidth
+//   );
+
+//   window.scrollTo({
+//     right: s1coords.top + window.pageYOffset,
+//     left: s1coords.left + window.pageXOffset,
+//     behavior: 'smooth',
+//   });
+// });
+
+//// new way of doing it
+
+// section1.scrollIntoView({ behavior: 'smooth' });
+
+/// Scrolling
+// window.scrollTo(
+//   s1coords.left + window.pageXOffset,
+//   s1coords.top + window.pageYOffset
+// );
+
+// window.scrollTo({
+//   right: s1coords.top + window.pageYOffset,
+//   left: s1coords.left + window.pageXOffset,
+//   behavior: 'smooth',
+// });
+
 /////// Types of events and Events handlers //////////
 
-// section1.scrollIntoview({ behavior: 'smooth' });
+// const h1 = document.querySelector('h1');
 
-const h1 = document.querySelector('h1');
+// // h1.onmouseenter = function (e) {
+// //   alert('addEventListener: You are reading the heading :D');
+// // };
 
-// h1.onmouseenter = function (e) {
-//   alert('addEventListener: You are reading the heading :D');
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great! You are reading the heading');
 // };
 
-const alertH1 = function (e) {
-  alert('addEventListener: Great! You are reading the heading');
+// h1.addEventListener('mouseenter', alertH1);
 
-};
-
-h1.addEventListener('mouseenter', alertH1);
-
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 /// <h1 onlick="alert"('HTML alert')"> alert en html
 
 ///// Event bubbling and capturing
+
+///// Event propagation in practices
