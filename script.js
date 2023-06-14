c32662e03c492b4a052f61adfc8e9438dd453ac7
Martin Quevedo <martinqs@live.com.ar>
2023-06-14 20:35:40 -0300

    Revealing elements

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ const navheight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   nav.classList.add('sticky');
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
@@ -199,6 +199,30 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `${navheight}px`,
 });
 headerObserver.observe(header);
+
+////////// Revealing Elements as you scroll down //
+// Reveal section
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
