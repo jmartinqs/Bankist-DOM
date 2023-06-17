2fe181b7886f0e360292b556883a6b24ffe2d265
Martin Quevedo <martinqs@live.com.ar>
2023-06-17 19:33:41 -0300

    dom cycle

--- script.js
+++ script.js
@@ -347,8 +347,20 @@ sliders();
 //////// DOM Lifecycle elements
 
 document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML parse and DOM tree build!');
+  console.log('HTML parse and DOM tree build!', e);
 });
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// msg that pops warning of page closure
+// windows.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
+
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
 
